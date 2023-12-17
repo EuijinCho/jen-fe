1874b1a4d7ee6a01f27cf4749dc9fa8725e8f25d
tony.cho <tony.cho@wavebridge.com>
2023-12-17 20:18:31 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("c");
+  console.log("a");
   return (
     <div className="App">
       <header className="App-header">
